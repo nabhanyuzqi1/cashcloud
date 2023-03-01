f293c6e8a9fb909317c52a2a607a7468965984e1
Nabhan Yuzqi <nabhanyuzqi1@gmail.com>
2023-03-02 03:45:10 +0700

    test: deployment

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -7,7 +7,7 @@ fun main() {
     document.body!!.appendChild(container)
 
     val welcome = Welcome.create {
-        name = "Kotlin/JS"
+        name = "Cash Cloud - POS"
     }
     createRoot(container).render(welcome)
 }
